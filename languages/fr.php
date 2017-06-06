eaa929b7b23eb19e439363eb287735a7dac8f8b3
Mathieu Etienne <thiousi@gmail.com>
2017-06-06 14:36:59 +0200

    Create fr translation

---
+++ languages/fr.php
@@ -0,0 +1,14 @@
+<?php
+l::set('plugin.sharingbuttons.facebook', 'Partager sur Facebook');
+l::set('plugin.sharingbuttons.twitter', 'Partager sur Twitter');
+l::set('plugin.sharingbuttons.google', 'Partager sur Google+');
+l::set('plugin.sharingbuttons.tumblr', 'Partager sur Tumblr');
+l::set('plugin.sharingbuttons.email', 'Partager via E-Mail');
+l::set('plugin.sharingbuttons.pinterest', 'Partager sur Pinterest');
+l::set('plugin.sharingbuttons.linkedin', 'Partager sur LinkedIn');
+l::set('plugin.sharingbuttons.reddit', 'Partager sur Reddit');
+l::set('plugin.sharingbuttons.xing', 'Partager sur XING');
+l::set('plugin.sharingbuttons.whatsapp', 'Partager sur WhatsApp');
+l::set('plugin.sharingbuttons.hackernews', 'Partager sur Hacker News');
+l::set('plugin.sharingbuttons.vk', 'Partager sur VK');
+l::set('plugin.sharingbuttons.telegram', 'Partager sur Telegram');
