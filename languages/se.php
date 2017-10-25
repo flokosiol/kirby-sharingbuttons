d0f1374df12edc420e456c64cb2df1898766cc68
flokosiol <git@flokosiol.de>
2017-10-25 22:51:20 +0200

    swedish translations

---
+++ languages/se.php
@@ -0,0 +1,17 @@
+<?php
+
+// Copy the content of this files to site/languages/fr.php and adjust it to your needs
+
+l::set('plugin.sharingbuttons.facebook', 'Dela på Facebook');
+l::set('plugin.sharingbuttons.twitter', 'Dela på Twitter');
+l::set('plugin.sharingbuttons.google', 'Dela på Google+');
+l::set('plugin.sharingbuttons.tumblr', 'Dela på Tumblr');
+l::set('plugin.sharingbuttons.email', 'Dela med e-post');
+l::set('plugin.sharingbuttons.pinterest', 'Dela på Pinterest');
+l::set('plugin.sharingbuttons.linkedin', 'Dela på LinkedIn');
+l::set('plugin.sharingbuttons.reddit', 'Dela på Reddit');
+l::set('plugin.sharingbuttons.xing', 'Dela på XING');
+l::set('plugin.sharingbuttons.whatsapp', 'Dela på WhatsApp');
+l::set('plugin.sharingbuttons.hackernews', 'Dela på Hacker News');
+l::set('plugin.sharingbuttons.vk', 'Dela på VK');
+l::set('plugin.sharingbuttons.telegram', 'Dela på Telegram');
